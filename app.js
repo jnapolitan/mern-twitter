711b35a47585bc4ddb10fc46ad87393374e3640d
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-26 16:10:07 -0800

    Initial mongoose db and express server setup

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mangoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send('Hello again world!'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
