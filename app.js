39dfb00a55ee6486b66d83e5da843e4ce8a5afad
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-26 23:12:25 -0800

    Set up passport config

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -19,5 +20,7 @@ app.get("/", (req, res) => {
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(passport.initialize());
+require('./config/passport')(passport);
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,18 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+router.get("/current", passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({
+        id: req.user.id,
+        handle: req.user.handle,
+        email: req.user.email
+    });
+});
 
 router.post("/register", (req, res) => {
     User.findOne({ email: req.body.email })
@@ -27,6 +37,38 @@ router.post("/register", (req, res) => {
                     });
                 });
             }
+        });
+});
+
+router.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({email})
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({email: 'No user with that email exists'});
+            } 
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch) {
+                        const payload = { id: user.id, handle: user.handle };
+
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            {expiresIn: 3600},
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                            });
+                    } else {
+                        return res.status(400).json({password: 'Incorrect password'});
+                    }
+                });
         });
 });
 
