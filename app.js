24cec11d82c7b88b52d9889ece41e1bcbc03080f
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-26 16:24:12 -0800

    Set up initial express test routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mangoURI;
+const port = process.env.PORT || 5000;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -9,7 +13,8 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send('Hello again world!'));
-
-const port = process.env.PORT || 5000;
-
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
