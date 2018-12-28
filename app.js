4fa16494ca15923d16ea238a6f626c02111d1f0b
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-28 14:52:34 -0800

    Add more heroku config

--- app.js
+++ app.js
@@ -7,15 +7,19 @@ const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send('Hello again world!')
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/public'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 app.use(bodyParser.urlencoded({ extended: false }));
