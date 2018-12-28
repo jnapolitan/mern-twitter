ee56e3b9f0857a7c607d496ad6701e9759d6b318
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-28 14:59:23 -0800

    More heroku config

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ mongoose
   .catch(err => console.log(err));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/public'));
+  app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
