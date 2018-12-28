79640c4866c3fda6f5fa76dc31a5bf51b11208ef
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-28 10:41:07 -0800

    Add session actions and auth routes

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class MainPage extends React.Component {
+
+    render() {
+        return (
+            <>
+                <h1>A Twitter Clone</h1>
+                <footer>
+                    Copyright %copy; 2019 Chirper
+                </footer>
+            </>
+        )
+    }
+}
